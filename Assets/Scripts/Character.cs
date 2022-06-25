34cab254871e2e7052ba1ffafa2becaa33f83f86
Samed <asbayrak34@gmail.com>
2022-06-25 18:27:32 +0300

    Added animations & horizontal movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,9 +6,20 @@ public class Character : MonoBehaviour
 {
     private CharacterController controller;
 
-    [SerializeField] float movementSpeed = 5f;
+    public float verticalMovementSpeed = 8f;
+    public float horizontalMovementSpeed = 4f;
+    public float rotationSpeed = 12f;
 
+    private Vector3 moveVec; // movement vector
+    private Vector3 verticalVelocity;
 
+    private bool canMove;
+
+    private Vector2 delta;
+    private Vector2 smoothDelta;
+    private Vector2 refVel = Vector2.zero;
+
+    private float lastPosX = 0;
 
 
 
@@ -19,7 +30,28 @@ public class Character : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPosX = Input.mousePosition.x;
+        }
 
+        if (Input.GetMouseButton(0))
+        {
+            controller.Move(transform.forward * Time.deltaTime * verticalMovementSpeed); // forward movement
+
+            delta.x = Input.mousePosition.x - lastPosX; // delta last frame
+
+            smoothDelta = Vector2.Lerp(smoothDelta, delta, 0.3f); // get smooth delta
+
+            moveVec = Vector3.Lerp(moveVec, smoothDelta.x * 0.2f * Vector3.right, Time.deltaTime * 11f); // lerp movement vector
+
+            if (moveVec != Vector3.zero)
+            {
+                controller.Move(moveVec * Time.deltaTime * horizontalMovementSpeed);
+            }
+
+            lastPosX = Input.mousePosition.x;
+        }
     }
 
     private void SetupComponents()
